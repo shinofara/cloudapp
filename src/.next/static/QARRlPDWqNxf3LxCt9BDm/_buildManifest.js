99ede128244275d1a86075aff426188893f9a9da
shinofara <shinofara+github@gmail.com>
2022-04-24 20:58:27 +0900

    Add: cd

---
+++ src/.next/static/QARRlPDWqNxf3LxCt9BDm/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/226dc341b7bfb363.css","static/chunks/pages/index-d99dcd7881c2f45c.js"],"/_error":["static/chunks/pages/_error-2aa47654017d7eab.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
